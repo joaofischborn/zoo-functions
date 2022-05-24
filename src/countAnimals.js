e0987d20131f2dfbefd60251f27d3634b6e887e6
João Vitor <joaovitorfischborn@icloud.com>
2022-05-24 15:28:53 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  const obj = {};
+  if (animal === undefined) {
+    species.forEach((specie) => {
+      obj[specie.name] = specie.residents.length;
+    });
+    return obj;
+  }
+  const { residents } = species.find((specie) => specie.name === animal.specie);
+
+  if (animal.sex) {
+    return residents.filter((resident) => resident.sex === animal.sex).length;
+  }
+  return residents.length;
 }
 
 module.exports = countAnimals;
