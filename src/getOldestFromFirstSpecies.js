50115788778af65efca6e49f3154d11af3ba626a
João Vitor <joaovitorfischborn@icloud.com>
2022-05-26 19:08:20 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const peoples = employees.filter((people) =>
+    id.includes(people.id)).map((responsible) => responsible.responsibleFor[0]);
+  const animals = species.find((animal) => animal.id.includes(peoples)).residents;
+  const ages = animals.reduce((acc, elemento) => elemento.age);
+  const result = animals.find((animal) => animal.age >= ages);
+  return [result.name, result.sex, result.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
