4c7e1982f710a7c6d0ca2d081dfc78d1cfb0fe72
João Vitor <joaovitorfischborn@icloud.com>
2022-05-27 16:09:45 -0300

    Requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,39 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const { employees, species } = data;
 
+const myEmployees = (worker) => employees.find((employe) => employe
+  .id === worker.id || employe.firstName === worker
+  .name || employe.lastName === worker.name);
+
+const mySpecies = (worker) => species.filter((specie) => {
+  if (!worker) {
+    throw new Error('Informações inválidas');
+  }
+  return worker.responsibleFor.includes(specie.id);
+});
+
+const allEmployees = () => employees.map((employee) => {
+  const getSpecie = mySpecies(employee);
+  return {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: getSpecie.map((specie) => specie.name),
+    locations: getSpecie.map((element) => element.location),
+  };
+});
+
+function getEmployeesCoverage(objeto) {
+  if (!objeto) {
+    return allEmployees();
+  }
+  const getEmploye = myEmployees(objeto);
+  const getSpecie = mySpecies(getEmploye);
+  return {
+    id: getEmploye.id,
+    fullName: `${getEmploye.firstName} ${getEmploye.lastName}`,
+    species: getSpecie.map((specie) => specie.name),
+    locations: getSpecie.map((element) => element.location),
+  };
+}
 module.exports = getEmployeesCoverage;
